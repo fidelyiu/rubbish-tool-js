a97b91e9ba3db8601811c052a2858fd3b9e574c5
Fidel Yiu <fidel-yiu@foxmail.com>
2023-01-31 21:01:18 +0800

    chore: 添加rollup配置

--- packages/rubbish-tool/rollup.config.ts
+++ packages/rubbish-tool/rollup.config.ts
@@ -1,10 +1,77 @@
 import type { RollupOptions } from 'rollup'
+import { defineConfig } from 'rollup'
+import path from 'node:path'
 import typescript from '@rollup/plugin-typescript'
+import terser from '@rollup/plugin-terser'
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const config: RollupOptions = {
-    input: 'src/index.ts',
-    output: { dir: 'dist' },
-    plugins: [typescript()],
-}
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
-export default config
+const input = path.join(__dirname, 'src/index.ts')
+
+const bundles: RollupOptions[] = [
+    /* commonjs */
+    {
+        input,
+        output: {
+            file: path.join(__dirname, 'dist/cjs/rubbish-tool.cjs'),
+            format: 'cjs',
+        },
+        plugins: [typescript()],
+    },
+    /* commonjs min */
+    {
+        input,
+        output: {
+            file: path.join(__dirname, 'dist/cjs/rubbish-tool.min.cjs'),
+            format: 'cjs',
+            // sourcemap: true,
+            // sourcemapFile: path.join(
+            //     __dirname,
+            //     'dist/cjs/rubbish-tool.min.map'
+            // ),
+        },
+        plugins: [typescript(), terser()],
+    },
+    /* es */
+    {
+        input,
+        output: {
+            file: path.join(__dirname, 'dist/esm/rubbish-tool.js'),
+            format: 'esm',
+        },
+        plugins: [typescript()],
+    },
+    /* es min */
+    {
+        input,
+        output: {
+            file: path.join(__dirname, 'dist/esm/rubbish-tool.min.js'),
+            format: 'esm',
+        },
+        plugins: [typescript(), terser()],
+    },
+    /* umd */
+    {
+        input,
+        output: {
+            name: 'RTool',
+            file: path.join(__dirname, 'dist/umd/rubbish-tool.js'),
+            format: 'umd',
+        },
+        plugins: [typescript()],
+    },
+    /* umd min */
+    {
+        input,
+        output: {
+            name: 'RTool',
+            file: path.join(__dirname, 'dist/umd/rubbish-tool.min.js'),
+            format: 'umd',
+        },
+        plugins: [typescript(), terser()],
+    },
+]
+
+export default defineConfig(bundles)
