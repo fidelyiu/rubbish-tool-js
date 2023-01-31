86b0f04142acead0d13fa517877397ade5d193bc
Fidel Yiu <fidel-yiu@qq.com>
2023-01-31 13:20:21 +0800

    chore: 添加rollup依赖

---
+++ packages/rubbish-tool/rollup.config.ts
@@ -0,0 +1,10 @@
+import type { RollupOptions } from 'rollup'
+import typescript from '@rollup/plugin-typescript'
+
+const config: RollupOptions = {
+    input: 'src/index.ts',
+    output: { dir: 'dist' },
+    plugins: [typescript()],
+}
+
+export default config
