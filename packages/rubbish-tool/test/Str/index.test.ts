f797424046d406e7776c5cefef5086c6ed50bf6b
Fidel Yiu <fidel-yiu@qq.com>
2023-02-02 10:45:40 +0800

    chore: 添加单元测试

--- packages/rubbish-tool/test/Str/index.test.ts
+++ packages/rubbish-tool/test/Str/index.test.ts
@@ -1,5 +1,21 @@
-import { strGetFirst } from '../../src/Str'
+import { strGetDelEnd, strGetFirst, strGetReverse } from '../../src/Str'
 
-test('adds 1 + 2 to equal 3', () => {
+test('字符串测试 翻转 有效值传入', () => {
+    expect(strGetReverse('Fidel Yiu')).toBe('uiY lediF')
+})
+
+test('字符串测试 首字符串 有效值传入', () => {
     expect(strGetFirst('Fidel Yiu')).toBe('F')
+})
+
+test('字符串测试 首字符串 无效值传入', () => {
+    expect(strGetFirst('')).toBe('')
+})
+
+test('字符串测试 删除结尾 存在结尾', () => {
+    expect(strGetDelEnd('Fidel Yiu', ' Yiu')).toBe('Fidel')
+})
+
+test('字符串测试 删除结尾 不存在结尾', () => {
+    expect(strGetDelEnd('Fidel Yiu', ' Hello')).toBe('Fidel Yiu')
 })
